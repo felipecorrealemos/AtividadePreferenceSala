828e79d06aa323a84fd936dac9f3dbadca7d159c
felipe <FelipeCorreaLemos@WIN-D8I71GMGMD7>
2025-10-24 11:12:40 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadePreferenceSala/Components/Services/AtividadeService.cs
@@ -0,0 +1,33 @@
+﻿using AtividadePreferenceSala.Components.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePreferenceSala.Components.Services
+{
+    public class AtividadeService
+    {
+        private int LIMITE = 5;
+
+        public List<Atividade> Carregar()
+        {
+            var lista = new List<Atividade>();
+
+            for (int i = 1; i <= LIMITE; i++)
+            {
+                var nome = Preferences.Get($"Atividade{i}", "");
+
+                if (!string.IsNullOrEmpty(nome))
+                {
+                    lista.Add(new Atividade { nome = nome });
+                }
+            }
+
+            return lista;
+        }
+
+
+    }
+}
